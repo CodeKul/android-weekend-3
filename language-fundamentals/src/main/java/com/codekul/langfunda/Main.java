59821703a09f94e5adfc02da7d43f3923cb654ed
aniruddha <aniruddha.kudalkar@gmail.com>
2016-07-23 18:01:46 +0530

    this is initial setup with 2 modules i.e. language fundamentals and inheritance

---
+++ language-fundamentals/src/main/java/com/codekul/langfunda/Main.java
@@ -0,0 +1,11 @@
+package com.codekul.langfunda;
+
+/**
+ * Created by aniruddha on 23/7/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java fundamentals");
+    }
+}
---
+++ src/main/java/com/codekul/brushup/Main.java
@@ -0,0 +1,24 @@
+package com.codekul.brushup;
+
+/**
+ * Created by aniruddha on 23/7/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        //javac Main.java -> invoking compiler
+        //java Main -> invoking interpreter
+
+        //Main.main();
+        System.out.println("It is working");
+    }
+
+    public static void main1(String[] args){
+
+    }
+}
+
+class My1 {
+
+}
